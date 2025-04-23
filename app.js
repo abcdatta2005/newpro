d6c91d4b75242a7aae27554f110effcf4be687fb
abcdatta2005 <151520879+abcdatta2005@users.noreply.github.com>
2025-04-23 11:06:47 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import FocusText from "./focus";
+import HoverBlur from "./hover";
+
+function App(){
+  return (
+    <div className="App">
+    <FocusText />
+    <HoverBlur/>
+    </div>
+
+  )}
+
+
+export default App;
